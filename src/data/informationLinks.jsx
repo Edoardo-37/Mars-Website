9644ddd6540e3a80e21a62b2b31fd3f51081d480
edo37 <edoardo0307@gmail.com>
2025-10-23 16:38:49 +0200

    Added information footer links data for dynamic mapping

---
+++ src/data/informationLinks.jsx
@@ -0,0 +1,24 @@
+const informationLinks = [
+  {
+    id: 0,
+    label: "Chi siamo",
+    link: "#",
+  },
+  {
+    id: 1,
+    label: "Termini e Condizioni",
+    link: "#",
+  },
+  {
+    id: 2,
+    label: "Privacy policy",
+    link: "#",
+  },
+  {
+    id: 3,
+    label: "Cookie policy",
+    link: "#",
+  },
+];
+
+export default informationLinks;
