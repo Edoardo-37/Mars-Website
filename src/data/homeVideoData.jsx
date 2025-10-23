3e3d66c8bcee3e13740f058cead75728227c4aad
edo37 <edoardo0307@gmail.com>
2025-10-23 16:39:35 +0200

    Added video data for dynamic mapping in home page

---
+++ src/data/homeVideoData.jsx
@@ -0,0 +1,19 @@
+const homeVideoData = [
+  {
+    id: 0,
+    videoLink: "https://www.youtube-nocookie.com/embed/aQhElFs5B48?si=KlGgo0-XEc8G2S8i",
+    title: "Le tappe del rover Perseverance della NASA su Marte - 2021",
+  },
+  {
+    id: 1,
+    videoLink: "https://www.youtube-nocookie.com/embed/kGvyQpfYsTg?si=XipODibwaJsFqC13",
+    title: "Esplora la superficie di Marte a 360°",
+  },
+  {
+    id: 2,
+    videoLink: "https://www.youtube-nocookie.com/embed/3gxu3Vbb9C0?si=QRdDKqnmIHpb_xXz",
+    title: "Atmosfera e acqua marziana: è possibile la vita su Marte?",
+  },
+];
+
+export default homeVideoData;
