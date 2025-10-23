6fcd4fa05a0888236a326c1b8f8cdad19f77f1a6
edo37 <edoardo0307@gmail.com>
2025-10-23 16:38:27 +0200

    Added navigation links data for dynamic mapping

---
+++ src/data/navLinks.jsx
@@ -0,0 +1,29 @@
+const navLinks = [
+  {
+    id: 0,
+    label: "Tecnologie",
+    link: "/tecnologie-di-marte",
+  },
+  {
+    id: 1,
+    label: "I Pericoli Del Viaggio",
+    link: "/pericoli-del-viaggio",
+  },
+  {
+    id: 2,
+    label: "Il Viaggio",
+    link: "/il-viaggio",
+  },
+  {
+    id: 3,
+    label: "I Pericoli Di Marte",
+    link: "/pericoli-di-marte",
+  },
+  {
+    id: 4,
+    label: "Sopravvivenza Su Marte",
+    link: "/sopravvivenza-su-marte",
+  },
+];
+
+export default navLinks;
